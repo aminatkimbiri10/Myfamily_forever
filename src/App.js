9e9d1eabd1e22dabc68b8905682aa83686c3c016
aminatkimbiri10 <user.name>
2023-12-21 17:20:45 +0100

    Netoyage

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+
+function Family({Data}) {
+  return (<div>
+    APP REACT
+  </div>)
+}  
+
+export {Data};
+export default Family;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Family, {Data} from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  Data.map((v)=>{
-    return (<Family nom={v.nom}/>)
-  })
   <React.StrictMode>
-    
+    <App/>
   </React.StrictMode>
 );
 
