0062f2b3792e33ba3ba9f82cc1bff4c4f2e18849
aminatkimbiri10 <user.name>
2024-01-15 17:09:24 +0100

    React initial

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-
-function Family({Data}) {
-  return (<div>
-    APP REACT
-  </div>)
-}  
-
-export {Data};
-export default Family;
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+export default App;
