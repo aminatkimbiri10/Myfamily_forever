0fa7d34831354e09b1762c7a74102f751c8be297
aminatkimbiri10 <user.name>
2024-03-26 10:48:59 +0000

    added

--- src/ListArticle.jsx
+++ src/ListArticle.jsx
@@ -1,55 +1,62 @@
-import { useState } from "react"
+import { useRef,useReducer } from "react"
 
 function Article() {
-    const [id,setId] = useState()
-    const [desi,setDesi] = useState()
-    const [prix,setPrix] = useState()
-    const [articles,setArticles] = useState([])
-
-    function AddArticle() {
-        setArticles([...articles,{id:id,desi:desi,prix:prix}])
-        console.log(articles)
-    }
-    function ResetArticle(e) {
-        e.target.parentElement.remove()
-        articles.splice(articles.indexOf(e.target.value),1)
-
-    }
-    function Empty() {
-        setArticles([])
-    }
-
-    function HandleInput(e) {
-        switch (e.target.name) {
-            case 'id':
-                setId(e.target.value)
+    const id=useRef()
+    const desi=useRef()
+    const prix=useRef()
+    function Reducer(state,action) {
+        switch (action.type) {
+            case "ADD":
+                return [... state,{
+                    id: action.id,
+                    desi: action.desi,
+                    prix: action.prix
+                  }];
                 break;
-            case 'desi':
-                setDesi(e.target.value)
+            case "Reset":
+                return state.filter((item)=>item.id!==action.id);
                 break;
-            case 'prix':
-                setPrix(e.target.value)
+            case "Empty":
+                return [];
                 break;
         
+            default:
+                break;
         }
+    }
+
+    const [state,dispatch]=useReducer(Reducer,[])
+    function AddArticle() {
+        dispatch({
+            type:'ADD',
+            id:id.current.value,
+            desi:desi.current.value,
+            prix:prix.current.value
+        })
+    }
+    function ResetArticle(id_article) {
+        dispatch({type:'Reset',id:id_article})
+    }
+    function Empty() {
+        dispatch({type:'Empty'})
     }
     return(
         <>
         <form action="">
-        <label htmlFor="id">ID :<input type="number" name="id" id="id" onChange={HandleInput}  /></label><br />
-        <label htmlFor="desi">Désignation :<input type="text" name="desi" id="desi" onChange={HandleInput}  /></label><br />
-        <label htmlFor="prix"> Prix :<input type="number" name="prix" id="prix" onChange={HandleInput}  /></label><br />
+        <label htmlFor="id">ID :<input type="number" name="id" id="id" ref={id}  /></label><br />
+        <label htmlFor="desi">Désignation :<input type="text" name="desi" id="desi" ref={desi} /></label><br />
+        <label htmlFor="prix"> Prix :<input type="number" name="prix" id="prix" ref={prix}  /></label><br />
         <input type="button" value="Ajouter" onClick={AddArticle} />
-        <input type="button" value="Vider" onClick={Empty} />
+        <input type="button" value="Vider" onClick={Empty}  />
         </form>
         <h1>Liste Articles</h1>
         <ul>
-            {articles.map((item,index)=>{
+            {state.map((item,index)=>{
                 return(
                     
                 <li key={index}>
                    Id: {item.id} | Désignation: {item.desi} | Prix :{item.prix}
-                   <button type="reset" onClick={ResetArticle}>Annuler</button><hr />
+                   <button type="button" onClick={()=>ResetArticle(item.id)} className="reset">Annuler</button><hr />
                 </li> )
             })}
         </ul>
