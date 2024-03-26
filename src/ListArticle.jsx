b9ed2a1d825a8cff376c1124e58347becc5e81a3
aminatkimbiri10 <user.name>
2024-03-26 10:57:08 +0000

    added

--- src/ListArticle.jsx
+++ src/ListArticle.jsx
@@ -2,21 +2,12 @@ import { useState } from "react"
 
 function Article() {
     const [id,setId] = useState()
-    const [desi,setDesi] = useState()
-    const [prix,setPrix] = useState()
+    const [desi,setDesi] = useState('Iphone')
+    const [prix,setPrix] = useState(400)
     const [articles,setArticles] = useState([])
 
     function AddArticle() {
         setArticles([...articles,{id:id,desi:desi,prix:prix}])
-        console.log(articles)
-    }
-    function ResetArticle(e) {
-        e.target.parentElement.remove()
-        articles.splice(articles.indexOf(e.target.value),1)
-
-    }
-    function Empty() {
-        setArticles([])
     }
 
     function HandleInput(e) {
@@ -36,21 +27,17 @@ function Article() {
     return(
         <>
         <form action="">
-        <label htmlFor="id">ID :<input type="number" name="id" id="id" onChange={HandleInput}  /></label><br />
-        <label htmlFor="desi">Désignation :<input type="text" name="desi" id="desi" onChange={HandleInput}  /></label><br />
-        <label htmlFor="prix"> Prix :<input type="number" name="prix" id="prix" onChange={HandleInput}  /></label><br />
+        <label htmlFor="id">ID :<input type="number" name="id" id="id" onChange={HandleInput}  /></label>
+        <label htmlFor="desi">Désignation :<input type="text" name="desi" id="desi" onChange={HandleInput}  /></label>
+        <label htmlFor="prix"> Prix :<input type="number" name="prix" id="prix" onChange={HandleInput}  /></label>
         <input type="button" value="Ajouter" onClick={AddArticle} />
-        <input type="button" value="Vider" onClick={Empty} />
         </form>
         <h1>Liste Articles</h1>
         <ul>
-            {articles.map((item,index)=>{
-                return(
-                    
-                <li key={index}>
-                   Id: {item.id} | Désignation: {item.desi} | Prix :{item.prix}
-                   <button type="reset" onClick={ResetArticle}>Annuler</button><hr />
-                </li> )
+            {articles.map((item)=>{
+                return(<li key={item.id}>
+                    {item.id}
+                </li>)
             })}
         </ul>
         </>
